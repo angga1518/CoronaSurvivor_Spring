d9285b28afdb7794ff8e53fa10fee1a22d4a486c
muhammad.erlangga <muhammad.erlangga@pinhome.id>
2021-02-02 00:17:03 +0700

    feat: connect to cloud firestore

---
+++ src/main/java/corona/survivor/spring/controller/PatientController.java
@@ -0,0 +1,36 @@
+package corona.survivor.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import corona.survivor.spring.model.Patient;
+import corona.survivor.spring.service.PatientService;
+
+import java.util.concurrent.ExecutionException;
+
+@RestController
+public class PatientController {
+
+    @Autowired
+    PatientService patientService;
+
+    @GetMapping("/getPatientDetails")
+    public Patient getPatient(@RequestParam String name) throws InterruptedException, ExecutionException {
+        return patientService.getPatientDetails(name);
+    }
+
+    @PostMapping("/createPatient")
+    public String createPatient(@RequestBody Patient patient) throws InterruptedException, ExecutionException {
+        return patientService.savePatientDetails(patient);
+    }
+
+    @PutMapping("/updatePatient")
+    public String updatePatient(@RequestBody Patient patient) throws InterruptedException, ExecutionException {
+        return patientService.updatePatientDetails(patient);
+    }
+
+    @DeleteMapping("/deletePatient")
+    public String deletePatient(@RequestParam String name) {
+        return patientService.deletePatient(name);
+    }
+}
---
+++ src/main/java/corona/survivor/spring/model/Patient.java
@@ -0,0 +1,47 @@
+package corona.survivor.spring.model;
+
+import org.springframework.stereotype.Component;
+
+@Component()
+public class Patient {
+
+    private String name;
+
+    private int age;
+
+    private String city;
+
+    public Patient(){
+        super();
+    }
+
+    public Patient(String name, int age, String city) {
+        this.name = name;
+        this.age = age;
+        this.city = city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
